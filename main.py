25e4a887989b96784deeaefcb9cbb0d11c897386
shirshir05 <shirshir05@walla.co.il>
2021-01-12 13:39:48 +0200

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import scipy.stats as st
 import seaborn as sn
 
-np.random.seed(42)
+np.random.seed(20)
 
 
 class Simulation:
@@ -25,40 +25,48 @@ class Simulation:
         self.get_data()
         # 1 B
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 1 B ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        # TODO add
+
         self.statical()
 
         self.remove_future()
 
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2 A ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        # TODO change days
         # simulation 1
         self.create_window(days=880, interval=10, number_simulation=100)
         self.windows.to_csv('1_windows.csv', index=False)
 
-        profit = self.calculate_profit(start_money_arg=100, built_in=False)
+        profit = self.calculate_profit( built_in=False)
+        self.calculate("2A", profit)
+
         self.check_question(profit, '1', built_in=False)
 
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2 B ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
         self.create_window(days=880, interval=10, number_simulation=100)
         self.windows.to_csv('2_windows.csv', index=False)
-        profit = self.calculate_profit(start_money_arg=100, built_in=True)
+
+        profit = self.calculate_profit( built_in=True)
+        self.calculate("2B", profit)
+
         self.check_question(profit, '2', built_in=True)
 
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2 C1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         # simulation 1
         self.create_window(days=880, interval=10, number_simulation=100)
         self.windows.to_csv('3_windows.csv', index=False)
-        profit = self.calculate_profit(start_money_arg=100, built_in=False)
+
+        profit = self.calculate_profit(built_in=False)
+        self.calculate("2C1", profit)
+
         self.check_question(profit, '3', built_in=False)
 
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2 C2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
         self.create_window(days=880, interval=10, number_simulation=100)
         self.windows.to_csv('4_windows.csv', index=False)
+        profit = self.calculate_profit(built_in=True)
+        self.calculate("2C2", profit)
 
-        profit = self.calculate_profit(start_money_arg=100, built_in=True)
         self.check_question(profit, '4', built_in=True)
 
     def get_data(self):
@@ -70,7 +78,6 @@ class Simulation:
             ticker = yfinance.Ticker(name)
             data = ticker.history(interval="1d", start="1980-01-01", end="2021-1-1")
             data[f'price_{name}'] = data['Close']
-            # TODO check multiply in 100
             data['return_%s' % (name)] = data['Close'].pct_change(1)
             self.tickets = self.tickets.join(data[['price_%s' % (name)]], how="outer").dropna()
             self.tickets = self.tickets.join(data[[f'return_{name}']], how="outer").dropna()
@@ -109,6 +116,12 @@ class Simulation:
         plt.ylabel("Percent")
         plt.savefig("File/DailyReturns/" + ticker.name + ".png")
 
+    def calculate(self, name, profit):
+        plt.figure(figsize=(10, 8))
+        plt.hist(profit, density=True)
+        plt.title(f"Histogram - Profit {name}")
+        plt.savefig("File/Profit/" + name + ".png")
+
     def remove_future(self):
         self.tickets = pd.DataFrame({})
         self.return_tickets = pd.DataFrame({})
@@ -127,7 +140,6 @@ class Simulation:
             window_add = []
             for window in range(size_window):
                 # found random index
-                # TODO check np.random
                 rand_index = np.random.randint(low=0, high=len(self.return_tickets) - interval)
                 random_days = self.return_tickets[rand_index: rand_index + interval]
                 window_add.extend(random_days.values)
@@ -140,9 +152,9 @@ class Simulation:
     def cumprod(tickets):
         com = ((tickets + 1).cumprod()).to_frame()
         com = com.applymap(lambda x: (x - 1) * 100)
-        return np.sum(com[com >= 36]) > 0, com
+        return np.sum(com[com > 36]) > 0, com
 
-    def calculate_profit(self, start_money_arg=100, built_in=False):
+    def calculate_profit(self, built_in=False):
         flag_over = False
         # profit = pd.DataFrame(columns=['VMC', 'EMR', 'CSX', 'UNP'])
         profit = []
@@ -176,7 +188,11 @@ class Simulation:
             if (list_add[0] + list_add[1] + list_add[2] + list_add[3]) / 4 < 0 and built_in:
                 profit.append(0)
             else:
-                profit.append((list_add[0] + list_add[1] + list_add[2] + list_add[3]) / 4)
+                add = (list_add[0] + list_add[1] + list_add[2] + list_add[3]) / 4
+                if isinstance(add, float):
+                    profit.append(add)
+                else:
+                    profit.append(add[0])
         return profit
 
     def check_question(self, profit, index, built_in=False):
@@ -186,14 +202,29 @@ class Simulation:
 
         std = df.std().values[0]
         mean = df.mean().values[0]
+        median = df.median().values[0]
         print(f"Mean = {mean}")
+        print(f"Median = {median}")
         print(f"Std = {std}")
-        print(f"0% profit {scipy.stats.norm(mean, std).pdf(0)*100}")
-        print(f"2% profit {scipy.stats.norm(mean, std).pdf(2)*100}")
-        print(f"(2%, 20%] {(scipy.stats.norm(mean, std).cdf(20)-scipy.stats.norm(mean, std).cdf(2))*100}")
-        print(f"(20%, 36%){(scipy.stats.norm(mean, std).cdf(36)-scipy.stats.norm(mean, std).cdf(20))*100}")
-        print(f"Confidence Interval { scipy.stats.norm.interval(0.9, loc=mean, scale=std)}")
+        if built_in:
+            print(f"0% profit {sum(i==0 for i in profit) / len(profit)}")
+            print(f"2% profit {sum(i == 2 for i in profit) / len(profit)}")
+        else:
+            print(f"0% profit {sum(-0.5 < i <0.5 for i in profit) / len(profit)}")
+            print(f"2% profit {sum(1.5 < i <2.5 for i in profit) / len(profit)}")
 
+        print(f"(2%, 20%] {sum(2 < i <= 20 for i in profit)/len(profit)}")
+        print(f"(20%, 36%){sum(20 < i < 36 for i in profit)/len(profit)}")
+
+        sort = sorted(profit)
+        sort = sort[6: len(sort) - 5]
+        print(f"Confidence Interval {sort[0]} - {sort[len(sort) -1]}")
+
+        # print(f"0% profit {scipy.stats.norm(mean, std).pdf(0)*100}")
+        # print(f"2% profit {scipy.stats.norm(mean, std).pdf(2)*100}")
+        # print(f"(2%, 20%] {(scipy.stats.norm(mean, std).cdf(20)-scipy.stats.norm(mean, std).cdf(2))*100}")
+        # print(f"(20%, 36%){(scipy.stats.norm(mean, std).cdf(36)-scipy.stats.norm(mean, std).cdf(20))*100}")
+        # print(f"Confidence Interval { scipy.stats.norm.interval(0.9, loc=mean, scale=std)}")
         # for key in profit.keys():
         #     template = {"Name Stock": key,
         #                 "Loss (<0)": np.round(np.sum(profit[key] < 0), 2),
